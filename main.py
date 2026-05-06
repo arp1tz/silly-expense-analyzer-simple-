04a560ab737048189590ae2b462aee04410a1236
arp1tz <co.2024.asshelke@bitwardha.ac.in>
2026-05-06 21:45:54 +0530

    v3: added JSON for data persistence

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-expenses = []
+import json
 
+def load_data():
+    try:
+        with open("expenses.json", "r") as file:
+            return json.load(file)
+    except:
+        return[]
+
+def save_data(expenses):
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file)  
+
+expenses = load_data()
 # -------- FUNCTIONS --------
 
 def add_expense(expenses):
     amount = int(input("Enter the amount spent: "))
     category = input("Enter the category: ").lower()
     expenses.append({'amount': amount, 'category': category})
+    save_data(expenses)
 
 
 def show_total(expenses):
